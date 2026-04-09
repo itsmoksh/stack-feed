4294d4aae8d0b77865cb68c5637dad488607e40d
Moksh Jain <mokshjain2005jain@gmail.com>
2026-04-09 11:49:19 +0530

    Changed extraction method in news

--- fetcher/news_fetcher.py
+++ fetcher/news_fetcher.py
@@ -1,7 +1,6 @@
 import feedparser
-from langchain_community.document_loaders import WebBaseLoader
 from datetime import datetime, timezone, timedelta
-
+import trafilatura
 
 def extract_news(rss_url: str, since_date: datetime):
     feeds = feedparser.parse(rss_url)
@@ -15,17 +14,21 @@ def extract_news(rss_url: str, since_date: datetime):
         if published_date > since_date:
             feed_links.append(entry['link'])
 
-    loader_multiple_pages  = WebBaseLoader(feed_links)
-    docs = loader_multiple_pages.load()
-    articles = {}
-    for doc in docs:
-        articles[doc.metadata['title']] = [doc.page_content, doc.metadata.get('source','')]
-    return articles
+    rss_blogs = {}
+    for link in feed_links:
+        loader = trafilatura.fetch_url(link)
+        content = trafilatura.extract(loader)
+        metadata = trafilatura.extract_metadata(loader)
+        rss_blogs[metadata.title] = {'content':content, 'source':metadata.url}
 
+    return rss_blogs
 
 since_date = datetime.now() - timedelta(days=7)
-news = extract_news('https://huggingface.co/blog/feed.xml',since_date)
+articles = extract_news('https://test/blog/feed.xml',since_date) #rss feed url here
 
-for title, news in news.items():
-    print(news[0])
-    break
+if __name__ == '__main__':
+    # Checking working of it.
+    for title, news in articles.items():
+        print(title)
+        print(news['content'])
+        print(news['source'])
